0f06e6f44316c43b3e876b0030ac7f1b7b8123d3
daim43012 <130775424+daim43012@users.noreply.github.com>
2024-03-20 22:19:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 let clickTimeout;
 let tokens = 0; // Переменная для отслеживания количества "токенов"
 
+let tokens1 = 1000; // Начальное значение
+const tokensMax = 1000; // Максимальное значение
+
+// Функция обновления полосы прогресса
+function updateProgressBar() {
+  let percentage = (tokens1 / tokensMax) * 100;
+  document.querySelector(".progressBarFill").style.width = percentage + "%";
+}
+
+// Инициализируем полосу прогресса при загрузке страницы
+updateProgressBar();
+
+// Установка интервала для автоматического восстановления значений
+setInterval(() => {
+  if (tokens1 < tokensMax) {
+    tokens1++; // Увеличиваем tokens на 1 каждую секунду
+    updateProgressBar(); // Обновляем полосу прогресса
+  }
+}, 1000); // 1000 миллисекунд (1 секунда)
+
 document.getElementById("img").addEventListener("click", function () {
   // Увеличиваем tokens на 1 при каждом клике
+
+  if (tokens > 0) {
+    tokens1--; // Уменьшаем tokens на 1 при каждом клике
+    updateProgressBar(); // Обновляем полосу прогресса
+  }
+
   tokens += 1;
   // Обновляем содержимое <h1> с новым значением tokens
-  document.querySelector(".count h1").textContent = `tokens: ${tokens}`;
+  document.querySelector(".count h1").textContent = `FTMC Tokens: ${tokens}`;
 
   // Вибрация при клике (поддерживается не на всех устройствах)
   if (navigator.vibrate) {
@@ -17,11 +43,9 @@ document.getElementById("img").addEventListener("click", function () {
   void this.offsetWidth; // Триггер перерисовки для повторного проигрывания анимации
   this.classList.add("clicked", "click-effect"); // Добавить класс для увеличения и класс для эффекта клика
 
-  // Через короткий промежуток времени убрать эффект "внутреннего клика", но оставить увеличение
   setTimeout(() => this.classList.remove("click-effect"), 50);
 
-  // Удаляем класс 'clicked' только тогда, когда перестанут кликать
   clickTimeout = setTimeout(() => {
     this.classList.remove("clicked");
-  }, 200); // Увеличьте время, если нужно больше времени на уменьшение
+  }, 200);
 });
