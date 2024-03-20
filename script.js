cdba918ef2aec9b38e31df16c45552843fcfd552
daim43012 <130775424+daim43012@users.noreply.github.com>
2024-03-21 01:50:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,63 +4,52 @@ let tokens1 = 1000; // Начальное значение
 let tokens2 = 1000; // Добавленная переменная
 const tokensMax = 1000; // Максимальное значение
 
-// Функция обновления полосы прогресса
 function updateProgressBar() {
   let percentage = (tokens1 / tokensMax) * 100;
   document.querySelector(".progressBarFill").style.width = percentage + "%";
 }
 
-// Инициализируем полосу прогресса при загрузке страницы
 updateProgressBar();
 
-// Установка интервала для автоматического восстановления значений
 setInterval(() => {
   if (tokens1 < tokensMax) {
     tokens1++; // Увеличиваем tokens1 на 1 каждую секунду
     tokens2++; // Увеличиваем tokens2 вместе с tokens1
     updateProgressBar(); // Обновляем полосу прогресса
-    // Обновляем отображаемое значение tokens2 в элементе .value h1
     document.querySelector(
       ".value h1"
     ).textContent = `⚡ ${tokens2} (+1) / ${tokensMax}`;
   }
-}, 1000); // 1000 миллисекунд (1 секунда)
+}, 1000);
 
 document.getElementById("img").addEventListener("mousedown", function (event) {
   event.preventDefault(); // Предотвращаем действие по умолчанию
 });
 
 document.getElementById("img").addEventListener("click", function () {
-  // Уменьшаем tokens1 и tokens2 при каждом клике, если tokens1 > 0
   if (tokens1 > 0) {
     tokens1--;
     tokens2--; // Уменьшаем tokens2 вместе с tokens1
     updateProgressBar(); // Обновляем полосу прогресса
 
-    // Создаём новый элемент для отображения изменения
     const changeElement = document.createElement("div");
-    changeElement.textContent = "+1"; // Текст, отображающий изменение
+    changeElement.textContent = "1"; // Текст, отображающий изменение
     changeElement.className = "token-change"; // Применяем класс с анимацией
 
-    // Получаем координаты клика относительно документа
     const clickX = event.clientX;
     const clickY = event.clientY;
 
-    // Учитываем прокрутку страницы
     changeElement.style.left = `${clickX + window.scrollX}px`;
     changeElement.style.top = `${clickY + window.scrollY}px`;
 
-    // Добавляем элемент к body
     document.body.appendChild(changeElement);
 
-    // Удаляем элемент после анимации
     setTimeout(() => {
       changeElement.remove();
-    }, 500); // Соответствует длительности анимации
+    }, 1000);
   }
 
-  tokens += 1; // Увеличиваем tokens на 1 при каждом клике
-  // Обновляем содержимое <h1> с новым значением tokens
+  tokens += 1;
   document.querySelector(".count h1").textContent = `FTMC Tokens: ${tokens}`;
   document.querySelector(
     ".value h1"
