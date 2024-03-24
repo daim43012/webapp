57098784b43f701bdf849383a452e4a5216baa63
daim43012 <130775424+daim43012@users.noreply.github.com>
2024-03-24 11:58:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let clickTimeout;
-let tokens = 0; 
-let progress_bar = 1000; 
-const tokensMax = 1000; 
+let tokens = 0;
+let progress_bar = 1000;
+const tokensMax = 1000;
 
 function updateProgressBar() {
   let percentage = (progress_bar / tokensMax) * 100;
@@ -21,8 +21,8 @@ fetchAndDisplayTgId();
 
 setInterval(() => {
   if (progress_bar < tokensMax) {
-    progress_bar++; 
-    updateProgressBar(); 
+    progress_bar++;
+    updateProgressBar();
     document.querySelector(
       ".value h1"
     ).textContent = `⚡ ${progress_bar} (+1) / ${tokensMax}`;
@@ -30,18 +30,18 @@ setInterval(() => {
 }, 1000);
 
 document.getElementById("img").addEventListener("mousedown", function (event) {
-  event.preventDefault(); 
+  event.preventDefault();
 });
 
 document.getElementById("img").addEventListener("click", function () {
   if (progress_bar > 0) {
     progress_bar--;
 
-    updateProgressBar(); 
+    updateProgressBar();
 
     const changeElement = document.createElement("div");
-    changeElement.textContent = "1"; 
-    changeElement.className = "token-change"; 
+    changeElement.textContent = "1";
+    changeElement.className = "token-change";
 
     const clickX = event.clientX;
     const clickY = event.clientY;
@@ -63,12 +63,12 @@ document.getElementById("img").addEventListener("click", function () {
   ).textContent = `⚡ ${progress_bar} (+1) / ${tokensMax}`;
 
   if (navigator.vibrate) {
-    navigator.vibrate(50); 
+    navigator.vibrate(50);
   }
 
-  clearTimeout(clickTimeout); 
-  this.classList.remove("click-effect"); 
-  void this.offsetWidth; 
+  clearTimeout(clickTimeout);
+  this.classList.remove("click-effect");
+  void this.offsetWidth;
   this.classList.add("clicked", "click-effect");
 
   clickTimeout = setTimeout(() => {
@@ -103,7 +103,7 @@ function fetchAndDisplayTgId() {
           progress_bar = data.tokens;
           updateProgressBar();
           document.getElementById(
-            "tokensValue"
+            "tokensfromdb"
           ).textContent = `⚡ ${tokens} (+1)`;
         } else {
           console.error("Ошибка при получении токенов:", data.message);
